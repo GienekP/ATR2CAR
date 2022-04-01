d01bc22d0406ab22b11a9c12600d1cf84b9ea7ae
GienekP <97046310+GienekP@users.noreply.github.com>
2022-04-01 18:28:57 +0200

    Update starter256.asm

--- starter256.asm
+++ starter256.asm
@@ -1,6 +1,6 @@
 ;-----------------------------------------------------------------------		
 ;
-; ATR2CAR starter for SWITCHABLE XEGS CARTRIDGE with 256B sectors
+; ATR2CAR starter for SWITCHABLE XEGS CARTRIDGE with 256 sectors
 ; (c) 2022 GienekP
 ;
 ;-----------------------------------------------------------------------
@@ -54,12 +54,12 @@ JSIOINT = $E459
 ;-----------------------------------------------------------------------		
 ; INITCART ROUTINE
 
-INIT	rts
+INIT		rts
 	
 ;-----------------------------------------------------------------------		
 ; CARTRUN ROUTINE
 	
-BEGIN	jsr IRQDIS
+BEGIN		jsr IRQDIS
 		jsr ROM2RAM
 		jsr SETRAM
 		jsr OVRDINT
@@ -71,7 +71,7 @@ BEGIN	jsr IRQDIS
 ;-----------------------------------------------------------------------		
 ; IRQ ENABLE
 
-IRQENB	lda #$40
+IRQENB		lda #$40
 		sta NMIEN
 		lda #$F7
 		sta IRQST
@@ -83,7 +83,7 @@ IRQENB	lda #$40
 ;-----------------------------------------------------------------------		
 ; IRQ DISABLE
 
-IRQDIS	sei	
+IRQDIS		sei	
 		lda #$00
 		sta DMACTL
 		sta NMIEN
@@ -94,7 +94,7 @@ IRQDIS	sei
 ;-----------------------------------------------------------------------		
 ; COPY ROM TO RAM
 	
-ROM2RAM	lda #$C0
+ROM2RAM		lda #$C0
 		sta TMP+1
 		ldy #$00
 		sty TMP
@@ -125,7 +125,7 @@ T1		cmp #$00
 ;-----------------------------------------------------------------------		
 ; SET RAM & DISABLE BASIC
 
-SETRAM	lda PORTB
+SETRAM		lda PORTB
 		and #$FE
 		ora #$02
 		sta PORTB
@@ -136,7 +136,7 @@ SETRAM	lda PORTB
 ;-----------------------------------------------------------------------		
 ; COPY NEW SIOINT PROCEDURE
 
-OVRDINT	lda #<SIOCPY
+OVRDINT		lda #<SIOCPY
 		sta TMP
 		lda #>SIOCPY
 		sta TMP+1
@@ -146,7 +146,7 @@ OVRDINT	lda #<SIOCPY
 		sta TMP+3
 			
 		ldy #ENDCPY-SIOCPY-1
-LPCPY	lda (TMP),Y
+LPCPY		lda (TMP),Y
 		sta (TMP+2),Y
 		dey
 		bne LPCPY
@@ -168,7 +168,7 @@ LPCPY	lda (TMP),Y
 ;-----------------------------------------------------------------------		
 ; COPY TO $RAMPROC FOR "KILLERS" PORTB
 
-RESERVE	lda #<ZEROCP
+RESERVE		lda #<ZEROCP
 		sta TMP
 		lda #>ZEROCP
 		sta TMP+1
@@ -177,7 +177,7 @@ RESERVE	lda #<ZEROCP
 		lda #>RAMPROC
 		sta TMP+3		
 		ldy #ZEROEND-ZEROCP-1
-RESCPY	lda (TMP),Y
+RESCPY		lda (TMP),Y
 		sta (TMP+2),Y
 		dey
 		bne RESCPY
@@ -188,7 +188,7 @@ RESCPY	lda (TMP),Y
 ;-----------------------------------------------------------------------		
 ; FINAL VALUES
 
-FINAL 	lda #$1F
+FINAL 		lda #$1F
 		sta MEMTOP
 		lda #$BC
 		sta MEMTOP+1
@@ -196,22 +196,22 @@ FINAL 	lda #$1F
 		sta RAMTOP
 		lda #$01
 		sta PDVMSK
-VCL1	lda VCOUNT
+VCL1		lda VCOUNT
 		cmp #$3
 		bne VCL1
-TSTMAX	lda VCOUNT
+TSTMAX		lda VCOUNT
 		cmp #$8A
 		bne VCL2
 		lda #$77
 		sta RAMPROC+4
-VCL2	cmp #$00
+VCL2		cmp #$00
 		bne TSTMAX
 		rts
 		
 ;-----------------------------------------------------------------------		
 ; LEAVE CART SPACE
 		
-BYEBYE	jmp RAMPROC+GOBOOT-ZEROCP
+BYEBYE		jmp RAMPROC+GOBOOT-ZEROCP
 
 ;-----------------------------------------------------------------------		
 ; SIO INTerface
@@ -236,7 +236,7 @@ SIO     = $E971
 		lda PDVMSK
 		beq FOUND
 		ldx #$08
-NEXT 	jsr RAMPROC	; jsr GETLOW
+NEXT 		jsr RAMPROC	; jsr GETLOW
 		beq END 	; beq FOUND
 		txa
 		pha
@@ -248,8 +248,8 @@ NEXT 	jsr RAMPROC	; jsr GETLOW
 		sta PDVRS
 		sta PDVREG
 		beq END
-FOUND 	jsr SIO
-END 	pla
+FOUND 		jsr SIO
+END 		pla
 		sta DUNIT
 		lda #$00
 		sta CRITIC
@@ -263,7 +263,7 @@ ENDCPY	; --->>> $C96D
 ;-----------------------------------------------------------------------		
 ; RELOC CODE FOR RAMPROC
 
-ZEROCP	lda VCOUNT
+ZEROCP		lda VCOUNT
 		cmp #$52		; $52->NTSC $77->PAL
 		bne ZEROCP		
 		ldy #$7F
@@ -272,24 +272,24 @@ ZEROCP	lda VCOUNT
 		
 		; --->>>CART<<<---
 
-CPYSEC	lda #$FF	; $010F
+CPYSEC		lda #$FF	; $010F
 		sta $D500
 		lda (TMP),Y
 		stx $D500
 		sta (TMP+2),Y
-MODE	dey
+MODE		dey
 		bpl CPYSEC
 		
 		ldy #$01
 		sty DSTATS	
 				
-BACK	lda #$FF	; $0123
+BACK		lda #$FF	; $0123
 		sta $D500
 		lda TRIG3
 		sta GINTLK
 		rts
 		
-GOBOOT	lda #$FF	; $012F
+GOBOOT		lda #$FF	; $012F
 		sta $D500
 		lda TRIG3
 		sta GINTLK
@@ -301,7 +301,7 @@ ZEROEND
 ;-----------------------------------------------------------------------		
 ; AROUND SIO INTerface
 
-AROUND	lda DCMND
+AROUND		lda DCMND
 		cmp #$52
 		beq SECREAD
 		cmp #$57
@@ -310,10 +310,10 @@ AROUND	lda DCMND
 		beq STATOK
 		cmp #$53
 		bne UNKWCMD
-STATOK	ldy #$01
+STATOK		ldy #$01
 		sty DSTATS
-UNKWCMD	jmp RAMPROC+BACK-ZEROCP
-SECREAD	lda DBYT
+UNKWCMD		jmp RAMPROC+BACK-ZEROCP
+SECREAD		lda DBYT
 		cmp #$80
 		beq SEC80	
 		ldy #$00
@@ -323,12 +323,12 @@ SECREAD	lda DBYT
 		sta RAMPROC+MODE-ZEROCP+1
 		clc
 		bcc CALCSEC		
-SEC80	ldy #$7F
+SEC80		ldy #$7F
 		lda #$88	; DEY
 		sta RAMPROC+MODE-ZEROCP
 		lda #$10	; BPL
 		sta RAMPROC+MODE-ZEROCP+1	
-CALCSEC lda #$00
+CALCSEC 	lda #$00
 		sta TMP
 		lda DAUX1
 		and #$1F
